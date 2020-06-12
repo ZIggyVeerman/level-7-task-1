8b850b831fb8f786b12393a0d090bb4fadcf2356
Ziggy Veerman <ziggy.veerman@hva.nl>
2020-06-12 17:13:53 +0200

    example finished

--- app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
+++ app/src/main/java/com/example/bottomnavigationbar/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.bottomnavigationbar
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
-
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,26 +17,35 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        initNavigation()
+    }
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+    private fun initNavigation() {
+        // The NavController
+        val navController = findNavController(R.id.navHostFragment)
+
+        // Connect the navHostFragment with the BottomNavigationView.
+        NavigationUI.setupWithNavController(navView, navController)
+
+        // Connect the navHostFragment with the Toolbar.
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
+
+        // Add a Destination Changed Listener. This gets called whenever the navigation controller is navigating to another fragment.
+        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+            when (destination.id) {
+                R.id.homeFragment -> showBottomNavigationBar(true)
+                R.id.rateFragment -> showBottomNavigationBar(true)
+                R.id.ratedFragment -> showBottomNavigationBar(false)
+            }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
-
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+    private fun showBottomNavigationBar(visible: Boolean) {
+        when (visible) {
+            true -> navView.visibility = View.VISIBLE
+            false -> navView.visibility = View.GONE
         }
     }
+
 }
